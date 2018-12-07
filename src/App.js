cd9df9d3deb476337aa05a83fa4446d5b3e8db13
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-12-07 17:02:54 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { withFormik } from "formik"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = ({ values, handleChange, handleSubmit }) => (
+  <div >
+    <form onSubmit={handleSubmit}>
+
+    <input
+      type="email"
+      name="email"
+      placeholder="Email..."
+      value={values.email}
+      onChange={handleChange}
+    />
+    <br />
+    <input
+      type="password"
+      name="password"
+      placeholder="Password..."
+      value={values.password}
+      onChange={handleChange}
+    />
+    <br />
+    <button type="submit">Submit</button>
+    </form>
+  </div>
+)
+
+const FormikApp = withFormik({
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    }
+  },
+  handleSubmit(values) {
+    console.log( values)
   }
-}
+})(App)
 
-export default App;
+export default FormikApp

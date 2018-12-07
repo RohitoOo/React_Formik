cdca5c06a9546f175e0446708a349e4c8514374b
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-12-07 18:13:28 -0500

    Validation With Yup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react"
 import { withFormik, Form, Field } from "formik"
+const  Yup = require( "yup")
 
-const App = ({values}) => (
+const App = ({
+
+  values,
+  errors,
+  touched
+
+}) => (
   <div >
   {/* {console.log(values)} */}
+  {touched.email && errors.email && <p>{errors.email}</p> }
     <Form>
     <Field
       type="email"
@@ -11,16 +19,25 @@ const App = ({values}) => (
       placeholder="Email..."
     />
     <br />
+    {touched.password && errors.password && <p>{errors.password}</p> }
     <Field
       type="password"
       name="password"
       placeholder="Password..."
     />
-    <Field
-      type="checkbox"
-      name="newsletter"
-      checked={Boolean(values.newsletter)}
-    />
+    <label>
+      <Field
+        type="checkbox"
+        name="newsletter"
+        checked={Boolean(values.newsletter)}
+      />
+      Join The Newsletter
+    </label>
+    <br />
+    <Field component="select" name="plan">
+      <option value="free">Free</option>
+      <option value="premium">Premium</option>
+    </Field>
     <br />
     <button type="submit">Submit</button>
     </Form>  
@@ -28,15 +45,20 @@ const App = ({values}) => (
 )
 
 const FormikApp = withFormik({
-  mapPropsToValues({ email, password, newsletter }) {
+  mapPropsToValues({ email, password, newsletter, plan }) {
     return {
       email: email || "",
       password: password || "",
-      newsletter:  newsletter || true
+      newsletter:  newsletter || true,
+      plan: plan || "premium"
     }
   },
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email().required(),
+    password: Yup.string().min(9).required()
+  }),
   handleSubmit(values) {
-    // console.log(values)
+    console.log(values)
   }
 })(App)
 
